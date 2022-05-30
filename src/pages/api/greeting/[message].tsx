56dfca2a34131928fa8512116068696cbfeb98a7
otajisan <taji.taji.otajisan@gmail.com>
2022-05-30 16:21:19 +0900

    :+1: add: SSR Authorizer (#3)
    * :+1: add: SSR Authorizer
    
    * :shower: rm: extra codes

--- src/pages/api/greeting/[message].tsx
+++ src/pages/api/greeting/[message].tsx
@@ -1,10 +1,24 @@
 import {NextApiHandler} from "next";
+import {withSSRContext} from "aws-amplify";
+import {SsrAuthorizer} from "../../../lib/ssr-authorizer";
 
 const GreetingMessageHandler: NextApiHandler = async (req, res) => {
+  const auth: any = await SsrAuthorizer(req, res);
+
+  const session = auth.signInUserSession;
+  const {payload} = session.getIdToken();
+  const cognitoGroupNames = payload['cognito:groups'];
+
+  console.log('========== Auth ==========');
+  console.log(auth);
+  console.log(cognitoGroupNames);
+  console.log('username: ' + auth.username);
+  console.log('==========================');
+
   const {message} = req.query;
 
   const responseBody = {
-    'message': message,
+    'message': message + ' ' + auth.username,
   }
 
   res.status(200).json(responseBody)
